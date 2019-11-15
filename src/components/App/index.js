33fc02256a789be7fdff0a5aaeda6865ba8c71dc
Daniel Andres Gallo Garcia <dgallo@teravisiontech.com>
2019-11-14 22:11:18 -0500

    [project-player] feat(router): define router page

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,12 +1,27 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { createGlobalStyle } from 'styled-components';
 /* Style Components */
 import { Container } from './styled';
+/* Components */
+import Cover from '../../pages/Cover';
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    margin: 0;
+  }
+`
 
 const App = () => {
   return (
-    <>
-      ...
-    </>
+      <Router>
+        <GlobalStyle />
+        <div className="container">
+          <Switch>
+            <Route path="/" exact component={Cover} />
+          </Switch>
+        </div>
+      </Router>
   )
 }
 
---
+++ src/pages/Cover.js
@@ -0,0 +1,13 @@
+import React from 'react'
+/* Style Components */
+import { Container } from './styled';
+
+const Cover = () => {
+  return (
+    <Container>
+      ...
+    </Container>
+  )
+}
+
+export default Cover
